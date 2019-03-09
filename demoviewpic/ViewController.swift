6b169b8d46c69c0b59f0c28f65607b474ec97c99
Daniel <babipapi@gmail.com>
2019-03-09 14:38:59 +0800

    Initial Commit

---
+++ demoviewpic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demoviewpic
+//
+//  Created by Daniel on 2019/3/9.
+//  Copyright © 2019 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
